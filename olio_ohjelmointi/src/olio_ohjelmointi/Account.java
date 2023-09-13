05e02c39f23e2aa216d82de38e45fe051bd8cefa
Jouni Kiviperä <jounikivipera@gmail.com>
2023-09-13 10:02:52 +0300

    m

--- olio_ohjelmointi/src/olio_ohjelmointi/Account.java
+++ olio_ohjelmointi/src/olio_ohjelmointi/Account.java
@@ -1,22 +1,26 @@
 package olio_ohjelmointi;
 
-import static java.lang.System.out;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class Account {
-	String name;
-	String address;
-	Double balance;
-	
-	public void display() {
-		out.print(name);
-		out.print("(");
-		out.print(address);
-		out.print(") has $");
-		out.print(balance);
-	}
-	
-	public double getInterest(double percentageRate) {
-		return balance * percentageRate / 100.00;
-	}
-	
+    String name;
+    String address;
+    Double balance;
+
+    public void display() {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+        DecimalFormat df = new DecimalFormat("0.00", symbols);
+
+        System.out.print(name);
+        System.out.print(" (");
+        System.out.print(address);
+        System.out.print(") has $");
+        System.out.print(df.format(balance));
+    }
+
+    public double getInterest(double percentageRate) {
+        return balance * percentageRate / 100.00;
+    }
 }
--- olio_ohjelmointi/src/olio_ohjelmointi/UseAccount.java
+++ olio_ohjelmointi/src/olio_ohjelmointi/UseAccount.java
@@ -1,35 +1,35 @@
 package olio_ohjelmointi;
 
-import static java.lang.System.out;
+import java.text.DecimalFormat;
 
 public class UseAccount {
-	public static void main(String[] args) {
-		Account myAccount;
-		Account yourAccount;
-		
-		myAccount = new Account();
-		yourAccount = new Account();
-		
-		myAccount.name ="Barry Burd";
-		myAccount.address = "222 testikatu";
-		myAccount.balance = 24.02;
-		
-		yourAccount.name = "Jane Q. Public";
-		yourAccount.address = "111 testitie";
-		yourAccount.balance = 55.63;
-		
-		myAccount.display();
-		out.print(" plus $");
-		out.print(myAccount.getInterest(5.00));
-		out.println(" interst ");
-		
-		yourAccount.display();
-		double yourInterestRate= 7.00;
-		out.print(" plus $");
-		double yourInterstAmount = yourAccount.getInterest(yourInterestRate);
-		out.print(yourInterstAmount);
-		out.println(" interst ");
-		
-		
-	}
+    public static void main(String[] args) {
+        Account myAccount;
+        Account yourAccount;
+
+        myAccount = new Account();
+        yourAccount = new Account();
+
+        myAccount.name = "Barry Burd";
+        myAccount.address = "222 testikatu";
+        myAccount.balance = 24.02;
+
+        yourAccount.name = "Jane Q. Public";
+        yourAccount.address = "111 testitie";
+        yourAccount.balance = 55.63;
+
+        System.out.print("Account details: ");
+        myAccount.display();
+        System.out.print(" plus $");
+        System.out.print(new DecimalFormat("0.00").format(myAccount.getInterest(5.00)));
+        System.out.println(" interest");
+
+        System.out.print("Account details: ");
+        yourAccount.display();
+        double yourInterestRate = 7.00;
+        System.out.print(" plus $");
+        double yourInterestAmount = yourAccount.getInterest(yourInterestRate);
+        System.out.print(new DecimalFormat("0.00").format(yourInterestAmount));
+        System.out.println(" interest");
+    }
 }

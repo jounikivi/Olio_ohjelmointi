a6f03702196d5b62939deb85665f06368cc24645
Jouni Kiviperä <jounikivipera@gmail.com>
2023-09-14 13:56:09 +0300

    muutoksia

--- java_olio2/src/java_olio2/Account.java
+++ java_olio2/src/java_olio2/Account.java
@@ -3,20 +3,45 @@ package java_olio2;
 import static java.lang.System.out;
 
 public class Account {
-	String name;
-	String address;
-	Double balance;
+	
+	private String name;
+	private String address;
+	private Double balance;
 	
 	public void display() {
-		out.print(name);
+		out.print(getName());
 		out.print("(");
-		out.print(address);
+		out.print(getAddress());
 		out.print(") has $");
-		out.print(balance);
+		out.print(getBalance());
 	}
 	
 	public double getInterest(double percentageRate) {
-		return balance * percentageRate / 100.00;
+		return getBalance() * percentageRate / 100.00;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 }

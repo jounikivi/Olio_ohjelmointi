6b290942854a78282e80729f3019be6472139f50
Jouni Kiviperä <jounikivipera@gmail.com>
2023-09-14 13:45:04 +0300

    m

---
+++ java_olio2/src/java_olio2/Account.java
@@ -0,0 +1,22 @@
+package olio_ohjelmointi;
+
+import static java.lang.System.out;
+
+public class Account {
+	String name;
+	String address;
+	Double balance;
+	
+	public void display() {
+		out.print(name);
+		out.print("(");
+		out.print(address);
+		out.print(") has $");
+		out.print(balance);
+	}
+	
+	public double getInterest(double percentageRate) {
+		return balance * percentageRate / 100.00;
+	}
+	
+}
---
+++ java_olio2/src/java_olio2/UseAccount.java
@@ -0,0 +1,35 @@
+package olio_ohjelmointi;
+
+import static java.lang.System.out;
+
+public class UseAccount {
+	public static void main(String[] args) {
+		Account myAccount;
+		Account yourAccount;
+		
+		myAccount = new Account();
+		yourAccount = new Account();
+		
+		myAccount.name ="Barry Burd";
+		myAccount.address = "222 testikatu";
+		myAccount.balance = 24.02;
+		
+		yourAccount.name = "Jane Q. Public";
+		yourAccount.address = "111 testitie";
+		yourAccount.balance = 55.63;
+		
+		myAccount.display();
+		out.print(" plus $");
+		out.print(myAccount.getInterest(5.00));
+		out.println(" interst ");
+		
+		yourAccount.display();
+		double yourInterestRate= 7.00;
+		out.print(" plus $");
+		double yourInterstAmount = yourAccount.getInterest(yourInterestRate);
+		out.print(yourInterstAmount);
+		out.println(" interst ");
+		
+		
+	}
+}

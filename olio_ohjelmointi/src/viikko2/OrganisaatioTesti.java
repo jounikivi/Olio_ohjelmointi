5776e95d52cb346ad1cc90e3e63b5f3a0574ada9
Jouni Kiviperä <jounikivipera@gmail.com>
2023-09-20 13:16:52 +0300

    muutoksia

--- olio_ohjelmointi/src/viikko2/OrganisaatioTesti.java
+++ olio_ohjelmointi/src/viikko2/OrganisaatioTesti.java
@@ -5,11 +5,20 @@ public class OrganisaatioTesti {
         Organization yritys1 = new Organization("XYZ Yritys", 100000.00, true);
         Organization yritys2 = new Organization("Avustusjärjestö", 50000.00, false);
         
-        System.out.println("Tiedot yritys 1;");
+        System.out.println("Tiedot yritys 1:");
         yritys1.displayInfo();
         
-        System.out.println("\nTiedot yritys 2:");
+        System.out.println("\nTiedot yritys 2: ");
         yritys2.displayInfo();
+        
+        //Esim: setterin käytöstä
+        yritys1.setNimi("Uusi Nimi");
+        yritys1.setVuosittainenTulo(120000.00);
+        yritys1.setVoittoaTavoitteleva(false);
+        
+        //Näytetään päivitetyt tiedot
+        System.out.println("\nPäivitetyt tiedot yritys 1;");
+        yritys1.displayInfo();
     }
 }
 
--- olio_ohjelmointi/src/viikko2/Organization.java
+++ olio_ohjelmointi/src/viikko2/Organization.java
@@ -1,26 +1,57 @@
 package viikko2;
 
 public class Organization {
-    private String nimi;
-    private double vuosittainenTulo;
-    private boolean voittoaTavoitteleva;
-
-    public Organization(String nimi, double vuosittainenTulo, boolean voittoaTavoitteleva) {
-        this.nimi = nimi;
-        this.vuosittainenTulo = vuosittainenTulo;
-        this.voittoaTavoitteleva = voittoaTavoitteleva;
-    }
-
-    public void näytäTiedot() {
-        System.out.println("Organisaation nimi: " + nimi);
-        System.out.println("Vuosittainen tulo: $" + vuosittainenTulo);
-        System.out.println("Voittoa tavoitteleva: " + (voittoaTavoitteleva ? "Kyllä" : "Ei"));
-    }
-    
-    public void displayInfo() {
-    	System.out.println("Organisaation nimi: " + nimi);
-    	System.out.println("Vuosittainen tulo: $" + vuosittainenTulo);
-    	System.out.println("Voittoa tavoitteleva: "+ (voittoaTavoitteleva ? "Kyllä" : "Ei"));
-    }
-}
+	private String nimi;
+	private double  vuosittainenTulo;
+	private  boolean voittoaTavoitteleva;
+	
+	 public Organization(String nimi, double vuosittainenTulo, boolean voittoaTavoitteleva) {
+	        this.nimi = nimi;
+	        this.vuosittainenTulo = vuosittainenTulo;
+	        this.voittoaTavoitteleva = voittoaTavoitteleva;
+	    }
+	 
+	 public String getNimi() {
+	        return nimi;
+	    }
+	 public void setNimi(String nimi) {
+		 if (nimi != null && !nimi.isEmpty()) {
+			 this.nimi = nimi;
+		 } else {
+			System.out.println("Virhe: Nimi ei voi olla tyhjä");
+		}
+	 }
+	 
+	 public double getVuosittainenTulo() {
+		 return vuosittainenTulo;
+	 }
+	 
+	 public void setVuosittainenTulo(double vuosittainenTulo) {
+		 if (vuosittainenTulo >= 0) {
+			 this.vuosittainenTulo = vuosittainenTulo;
+		 } else {
+			System.out.println("Virhe: Vuosittainen tulo ei voi alla negaatiivinen");
+		}
+	 }
+	 
+	 public boolean isVoittoaTavoitteleva() {
+		 return voittoaTavoitteleva;
+	 }
+	 
+	 public void setVoittoaTavoitteleva(boolean voittoaTavoitteleva) {
+		 this.voittoaTavoitteleva = voittoaTavoitteleva;
+	 }
+	 
+	 public void näytäTiedot() {
+	        System.out.println("Organisaation nimi: " + nimi);
+	        System.out.println("Vuosittainen tulo: $" + vuosittainenTulo);
+	        System.out.println("Voittoa tavoitteleva: " + (voittoaTavoitteleva ? "Kyllä" : "Ei"));
+	    }
+	    
+	    public void displayInfo() {
+	    	System.out.println("Organisaation nimi: " + nimi);
+	    	System.out.println("Vuosittainen tulo: $" + vuosittainenTulo);
+	    	System.out.println("Voittoa tavoitteleva: "+ (voittoaTavoitteleva ? "Kyllä" : "Ei"));
+	    }
+	}
 

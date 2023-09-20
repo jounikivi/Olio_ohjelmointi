6d3944f8ec241182f85b0850311a0ff194b15828
Jouni Kiviperä <jounikivipera@gmail.com>
2023-09-20 14:36:46 +0300

    m

--- olio_ohjelmointi/src/viikko2/Main.java
+++ olio_ohjelmointi/src/viikko2/Main.java
@@ -1,5 +1,19 @@
 package viikko2;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Ajastin time = new Ajastin();
+
+        while (!time.loppu()) {
+            System.out.println("Aika ei ole vielä loppu! Aika: " + time.aika());
+            time.lisaa();
+        }
+
+        System.out.println("Aika loppui! Aika: " + time.aika());
+        time.vahenna();
+        System.out.println("Aika ei ole loppu! Aika: " + time.aika());
+    }
 }
+

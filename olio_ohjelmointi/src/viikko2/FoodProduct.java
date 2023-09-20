8bfd31f05e9d6c58905e31b8496853af516a7da9
Jouni Kiviperä <jounikivipera@gmail.com>
2023-09-20 13:55:51 +0300

    Update FoodProduct.java

--- olio_ohjelmointi/src/viikko2/FoodProduct.java
+++ olio_ohjelmointi/src/viikko2/FoodProduct.java
@@ -15,7 +15,68 @@ public class FoodProduct {
         this.kaloreitaAnnostaKohti = kaloreitaAnnostaKohti;
     }
 
-    public void näytäTiedot() {
+    public String getTyyppi() {
+		return tyyppi;
+	}
+
+	public void setTyyppi(String tyyppi) {
+		if (tyyppi != null && !tyyppi.isEmpty()) {
+			this.tyyppi = tyyppi;
+		} else {
+			System.out.println("Virhe: Tyyppi ei voi olla tyhjä");
+		}
+	}
+
+	public double getPaino() {
+		return paino;
+	}
+
+	public void setPaino(double paino) {
+		if (paino >= 0) {
+			this.paino = paino;
+		} else {
+			System.out.println("Virhe: Paino ei voi olla tyhjä");
+		}
+	}
+
+	public double getHinta() {
+		return hinta;
+	}
+
+	public void setHinta(double hinta) {
+		if (hinta >= 0) {
+			this.hinta = hinta;
+		}
+		else {
+			System.out.println("Virhe: Hinta ei voi olla negatiivinen");
+		}
+	}
+
+	public int getAnnostenMaara() {
+		return annostenMaara;
+	}
+
+	public void setAnnostenMaara(int annostenMaara) {
+		if (annostenMaara >=0) {
+			this.annostenMaara = annostenMaara;
+		} else {
+			System.out.println("Virhe: Annosten määrä ei voi olla negatiivinen");
+		}
+	}
+
+	public double getKaloreitaAnnostaKohti() {
+		return kaloreitaAnnostaKohti;
+	}
+
+	public void setKaloreitaAnnostaKohti(double kaloreitaAnnostaKohti) {
+		if (kaloreitaAnnostaKohti >= 0) {
+			this.kaloreitaAnnostaKohti = kaloreitaAnnostaKohti;
+		} else {
+			System.out.println("Virhe: Kalorimäärä ei voi olla negatiivinen");
+		}
+	}
+
+	public void näytäTiedot() {
         System.out.println("Tuotetiedot:");
         System.out.println("Tyyppi: " + tyyppi);
         System.out.println("Paino: " + paino + " grammaa");

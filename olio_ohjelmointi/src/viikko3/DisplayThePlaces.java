869a2cbca3df2951c00530f3571e3f3996da5213
Jouni Kiviperä <jounikivipera@gmail.com>
2023-09-27 18:50:37 +0300

    muu

--- olio_ohjelmointi/src/viikko3/DisplayThePlaces.java
+++ olio_ohjelmointi/src/viikko3/DisplayThePlaces.java
@@ -1,28 +1,27 @@
 package viikko3;
 
 public class DisplayThePlaces {
-    public static void main(String[] args) {
-        // Luo PlaceToLive-instanssit suoraan koodissa
-        PlaceToLive place1 = new PlaceToLive("Osoite 1", 2, 1000); // Esimerkkidatat
-        PlaceToLive place2 = new PlaceToLive("Osoite 2", 3, 1500); // Esimerkkidatat
 
-        // Syötä asumiskustannukset
-        double costOfLiving = 2000.0; // Esimerkkidata
+	  public static void main(String[] args) {
 
-        // Näytä kustannukset
-        double costPerSquareUnit1 = place1.calculateCostPerSquareUnit(costOfLiving);
-        double costPerBedroom1 = place1.calculateCostPerBedroom(costOfLiving);
+	    // Luodaan asuinpaikkoja
+	    PlaceToLive house = new PlaceToLive("Kotikatu 12", 3, 120);
+	    PlaceToLive apartment = new PlaceToLive("Asuntokatu 23", 2, 70);
 
-        double costPerSquareUnit2 = place2.calculateCostPerSquareUnit(costOfLiving);
-        double costPerBedroom2 = place2.calculateCostPerBedroom(costOfLiving);
+	    // Näytetään tietoja asuinpaikoista
+	    System.out.println("** Asunto 1 **");
+	    System.out.println("Osoite: " + house.getAddress());
+	    System.out.println("Makuuhuoneita: " + house.getNumberOfBedrooms());
+	    System.out.println("Pinta-ala: " + house.getArea() + " neliömetriä");
 
-        System.out.println("Paikka 1: " + place1.getAddress());
-        System.out.println("Kustannus neliöjalkaa (tai neliömetriä) kohden: $" + costPerSquareUnit1);
-        System.out.println("Kustannus makuuhuonetta kohden: $" + costPerBedroom1);
+	    System.out.println();
 
-        System.out.println("Paikka 2: " + place2.getAddress());
-        System.out.println("Kustannus neliöjalkaa (tai neliömetriä) kohden: $" + costPerSquareUnit2);
-        System.out.println("Kustannus makuuhuonetta kohden: $" + costPerBedroom2);
-    }
-}
+	    System.out.println("** Asunto 2 **");
+	    System.out.println("Osoite: " + apartment.getAddress());
+	    System.out.println("Makuuhuoneita: " + apartment.getNumberOfBedrooms());
+	    System.out.println("Pinta-ala: " + apartment.getArea() + " neliömetriä");
+
+	  }
+
+	}
 
--- olio_ohjelmointi/src/viikko3/PlaceToLive.java
+++ olio_ohjelmointi/src/viikko3/PlaceToLive.java
@@ -1,38 +1,29 @@
 package viikko3;
 
 public class PlaceToLive {
-    private String address;
-    private int numberOfBedrooms;
-    private double area;
 
-    // Konstruktori
-    public PlaceToLive(String address, int numberOfBedrooms, double area) {
-        this.address = address;
-        this.numberOfBedrooms = numberOfBedrooms;
-        this.area = area;
-    }
+	  private String address;
+	  private int numberOfBedrooms;
+	  private double area;
 
-    // Metodi hinnan laskemiseksi neliöjalkaa (tai neliömetriä) kohden
-    public double calculateCostPerSquareUnit(double costOfLiving) {
-        return costOfLiving / area;
-    }
+	  public PlaceToLive(String address, int numberOfBedrooms, double area) {
+	    this.address = address;
+	    this.numberOfBedrooms = numberOfBedrooms;
+	    this.area = area;
+	  }
 
-    // Metodi hinnan laskemiseksi makuuhuonetta kohden
-    public double calculateCostPerBedroom(double costOfLiving) {
-        return costOfLiving / numberOfBedrooms;
-    }
+	  public String getAddress() {
+	    return address;
+	  }
 
-    // Getterit
-    public String getAddress() {
-        return address;
-    }
+	  public int getNumberOfBedrooms() {
+	    return numberOfBedrooms;
+	  }
 
-    public int getNumberOfBedrooms() {
-        return numberOfBedrooms;
-    }
+	  public double getArea() {
+	    return area;
+	  }
 
-    public double getArea() {
-        return area;
-    }
-}
+	}
+
 

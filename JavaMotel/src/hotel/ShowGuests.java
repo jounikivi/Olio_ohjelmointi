e871ee40f86ca879870e59ae6a8f227455538eb8
Jouni Kiviperä <jounikivipera@gmail.com>
2023-10-05 12:44:58 +0300

    Update ShowGuests.java

--- JavaMotel/src/hotel/ShowGuests.java
+++ JavaMotel/src/hotel/ShowGuests.java
@@ -1,5 +1,12 @@
 package hotel;
 
 public class ShowGuests {
-
+	public static void main(String[] args) {
+		int[] quest = new int [10];
+		
+		
+		for (int roomNum = 0; roomNum < 10; roomNum++) {
+			quest[roomNum] = diskScanner.nex.int()
+		}
+	}
 }

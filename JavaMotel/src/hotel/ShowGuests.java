5d1b310130a199b59fabec305d5f03a26dbc93b3
Jouni Kiviperä <jounikivipera@gmail.com>
2023-10-05 12:53:51 +0300

    Update ShowGuests.java

--- JavaMotel/src/hotel/ShowGuests.java
+++ JavaMotel/src/hotel/ShowGuests.java
@@ -1,12 +1,17 @@
 package hotel;
 
+import static java.lang.System.out;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
 public class ShowGuests {
 	public static void main(String[] args) {
 		int[] quest = new int [10];
-		
+		var diskScanner = new Scanner("questlist");
 		
 		for (int roomNum = 0; roomNum < 10; roomNum++) {
-			quest[roomNum] = diskScanner.nex.int()
+			quest[roomNum] = diskScanner.nex.int();
 		}
 	}
 }

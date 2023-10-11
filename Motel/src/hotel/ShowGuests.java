64684d2dd47711e5a918eb017a407d61f6085072
Jouni Kiviperä <jounikivipera@gmail.com>
2023-10-11 15:14:42 +0300

    m

--- Motel/src/hotel/ShowGuests.java
+++ Motel/src/hotel/ShowGuests.java
@@ -1,31 +1,27 @@
 package hotel;
 
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ShowGuests {
 
-	  public static void main(String[] args) {
-	    // Luo taulukko
-	    int[] rooms = new int[10];
+    public static void main(String[] args) throws FileNotFoundException {
+        int guests[] = new int[10];
+        Scanner diskScanner = new Scanner(new File("GuestList.txt"));
 
-	    try {
-	      // Lue tiedot tiedostosta
-	      Scanner input = new Scanner(new File("GuestList.txt"));
+        for (int roomNum = 0; roomNum < 10; roomNum++) {
+            guests[roomNum] = diskScanner.nextInt();
+        }
 
-	      for (int i = 0; i < rooms.length; i++) {
-	        rooms[i] = input.nextInt();
-	      }
-	    } catch (FileNotFoundException e) {
-	      // Tiedostoa ei löydy
-	      System.out.println("Tiedosto GuestList.txt ei löydy!");
-	    }
+        System.out.println("Huone\tMäärä");
 
-	    // Tulosta tiedot
-	    for (int i = 0; i < rooms.length; i++) {
-	      System.out.println("Huone " + (i + 1) + ": " + rooms[i]);
-	    }
-	  }
-	}
+        for (int roomNum = 0; roomNum < 10; roomNum++) {
+            System.out.print(roomNum);
+            System.out.print("\t");
+            System.out.println(guests[roomNum]);
+        }
 
+        diskScanner.close();
+    }
+}

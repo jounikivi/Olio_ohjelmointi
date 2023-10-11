028b1121b577772694765e7ce73cd7b8fec432b4
Jouni Kiviperä <jounikivipera@gmail.com>
2023-10-11 10:59:33 +0300

    Update TooManyWords.java

--- kt_5/src/kt_5/TooManyWords.java
+++ kt_5/src/kt_5/TooManyWords.java
@@ -1,9 +1,31 @@
 package kt_5;
 
-public class TooManyWords {
-	public static void main(String[] args) {
-		// luodaan taulukko tallentamaan sanojen määrä jokaisella sivulle
-		int[] words = {0, 296, 342, 405 ,363, 350, 323, 101};
-	}
+import java.util.Scanner;
 
+public class TooManyWords {
+
+  public static void main(String[] args) {
+    // Luodaan taulukko tallentamaan sanojen määrän jokaisella sivulla
+    int[] words = {0, 296, 342, 405, 363, 350, 323, 101};
+
+    // Luodaan Scanner-olio käyttäjän syötteen lukemiseen
+    Scanner scanner = new Scanner(System.in);
+
+    // Kysytään käyttäjältä sivunumero
+    System.out.println("Anna sivunumero: ");
+    int pageNumber = scanner.nextInt();
+
+    // Tarkistetaan, onko sivunumero kelvollinen
+    if (pageNumber < 1 || pageNumber > 7) {
+      System.out.println("Kelvoton sivunumero.");
+      System.exit(1);
+    }
+
+    // Haetaan sanojen määrä pyydetyltä sivulta
+    int wordCount = words[pageNumber - 1];
+
+    // Tulostetaan sanojen määrä pyydetyltä sivulta
+    System.out.println("Sivulla " + pageNumber + " on " + wordCount + " sanaa.");
+  }
 }
+

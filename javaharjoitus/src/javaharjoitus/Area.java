e70cbc224d5a991937ea2f97ad64e8d8e31c4c08
Jouni Kiviperä <jounikivipera@gmail.com>
2023-11-16 12:46:29 +0200

    m

---
+++ javaharjoitus/src/javaharjoitus/Area.java
@@ -0,0 +1,5 @@
+package javaharjoitus;
+
+public interface Area {
+
+}
---
+++ javaharjoitus/src/javaharjoitus/Shape.java
@@ -0,0 +1,16 @@
+package javaharjoitus;
+
+public abstract class Shape {
+	int size;
+	public Shape(int size) {
+		this.size = size;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	
+}
